c8f187aaa965e74e1b1a47eecddad88995293bd6
KielRodriguez <kr.rdz.20@gmail.com>
2017-01-21 15:26:11 -0600

    Se agrega redireccionamiento a accion show despues de gurardar

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,7 +3,13 @@ class ArticlesController < ApplicationController
   end
 
   def create
-      @article = Article.new(article_params)
+    @article = Article.new(article_params)
+      @article.save
+      redirect_to @article
+  end
+
+  def show
+    @article = Article.find(params[:id])
   end
 
   private
